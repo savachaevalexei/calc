1c6da2024c6328f2cd87914233285e9e0af8dfee
Savachaev Alexey <email@adress>
2024-03-20 06:14:18 +0300

    add simple logger

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
+
 class ComplexNumber:
     def __init__(self, real, imag):
         self.real = real
@@ -9,6 +13,7 @@ class ComplexNumber:
     def __add__(self, other):
         real = self.real + other.real
         imag = self.imag + other.imag
+        logging.info(f"Answer: {ComplexNumber(real, imag)}.")
         return ComplexNumber(real, imag)
      
     def __mul__(self, other):
@@ -24,18 +29,25 @@ class ComplexNumber:
 
 print(f"Пример ввода. Например, дано число 1+2i \n В программу необходимо передать в следующем виде: \n 1,2")
 a, b = map(int, input("Введите действительную и мнимую часть 1-го числа через запятую: ").replace(" ", "").split(","))
+logging.info(f"User enter {a} and {b}.")
 c, d = map(int, input("Введите действительную и мнимую часть 2-го числа через запятую: ").replace(" ", "").split(","))
+logging.info(f"User enter {c} and {d}.")
 operator = input("Введите знак математической операции: +, * или / ").replace(" ", "")
+logging.info(f"User enter {operator}.")
 
 z1 = ComplexNumber(a, b)
 z2 = ComplexNumber(c, d)
 
 if (operator == "+"):
     print("Сложение :", z1 + z2)
+
 elif (operator == "*"):
     print("Умножение :",  z1 * z2)
 elif (operator == "/"):
     print("Деление :",  z1 / z2)
 else:
+    logging.info("the operator was entered incorrectly")
     print("Введите корректный оператор!")
+
+
 
